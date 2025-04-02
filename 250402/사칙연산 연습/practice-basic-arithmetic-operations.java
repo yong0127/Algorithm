00fbb01f397a1dee298585777b298b371c8ebaf5
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-04-02 21:45:12 +0900

    Title: 사칙연산 연습, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250402/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+        int c = a+b;
+
+        System.out.println(c/8);        // 4
+        System.out.println(c%8);        // 3
+    }
+}
