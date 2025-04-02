54565033016a781a4d2bf8d2341dfbc93c29d3ad
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-04-02 22:23:15 +0900

    Title: 실수 받아 그대로 출력, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250402/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        Double inData = Double.parseDouble(sc.next());
+        System.out.printf("%.2f", inData);
+    }
+}
