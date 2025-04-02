7cc09bccd5dae42c4c0ec0e220fbc41911e3999f
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-04-02 22:19:29 +0900

    Title: 입력받아 계산, Time: 107ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250402/입력받아 계산/input-calculate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        int inData = Integer.parseInt(sc.next());
+        inData += 2;
+
+        System.out.print(inData);    
+    }
+}
