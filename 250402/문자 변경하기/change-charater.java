d185697ba76e52a65897fbbdae2b936473eaed68
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-04-02 21:35:18 +0900

    Title: 문자 변경하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250402/문자 변경하기/change-charater.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char a = 'c';
+        a = 'T';
+        System.out.print(a);
+        
+    }
+}
