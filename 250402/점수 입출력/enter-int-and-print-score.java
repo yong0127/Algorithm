cf179742d2dfaebaa6c23643d0a9a072f1a7f283
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-04-02 22:17:14 +0900

    Title: 점수 입출력, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250402/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int point = Integer.parseInt(sc.next());
+
+        System.out.printf("Your score is %d point.", point);
+    }
+}
