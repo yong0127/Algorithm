382b06aa2f42d996c10b863caf9aa1f980b09068
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-04-03 21:02:59 +0900

    Title: 입력받은 값과 합 출력, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250403/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner scr = new Scanner(System.in);
+        int inData1 = scr.nextInt();
+        int inData2 = scr.nextInt();
+        int sumData = inData1 + inData2;
+
+        System.out.print(inData1 + " " + inData2 + " " + sumData);
+    }
+}
