76e4ec3f90cd93ebe8af3139bb663e40bc52105e
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-04-03 21:33:00 +0900

    Title: 시험 통과 여부 확인하기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250403/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scr = new Scanner(System.in);
+
+        int score        = scr.nextInt();
+        int needMore      = 0;
+        String resultMsg = "";
+
+        if(score > 80) {
+            resultMsg = "pass";
+        } else {
+            needMore = 80 - score;
+            resultMsg = needMore + " more score";
+        }
+        System.out.print(resultMsg);
+    }
+}
