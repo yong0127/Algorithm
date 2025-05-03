520ba5a3d38010c19143dde8d815f5794077294f
yong0127 <68551458+yong0127@users.noreply.github.com>
2025-05-03 15:04:06 +0900

    Title: 최대공약수 구하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250503/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        System.out.print(findGcd(n,m));
+    }
+
+    public static int findGcd(int n, int m) {
+        
+        int a = Math.abs(n);
+        int b = Math.abs(m);
+
+        if(a == 0 && b== 0) return 0;
+        if(a == 0) return b;
+        if(b == 0) return a;
+
+        int minNum = Math.min(a,b);
+        int gcd = 0;
+
+        for(int i=minNum; i>1; i--) {
+            if(a%i==0 && b%i==0) {
+                gcd = i;    
+                break;
+            }
+        }
+        
+        return gcd;
+    }
+}
